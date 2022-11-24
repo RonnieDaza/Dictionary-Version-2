92dc179b554edec07289f5ef91d51aec2e08753c
RonnieDaza <daza.ronnie.09212002@gmail.com>
2022-11-24 09:48:56 +0800

    Add choice 1

--- Dictionary V2.py
+++ Dictionary V2.py
@@ -10,3 +10,11 @@ dict_info =  {"Mark": ["19", "Manila", "09123456789"]}
 while True:
 
     choice = int(input("What do you want to do? (1-3) "))
+    if choice == 1:
+        fullName = input("Name: ")
+        age = int(input("Age: "))
+        address = input("Address: ")
+        country = input("Country: ")
+        phoneNumber = input("Phone Number: ")
+        gender = input("Gender: ")
+        email = input("Email: ")

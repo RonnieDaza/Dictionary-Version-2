8432b9fa28bec44c3b7f6152a9d4e5085735285b
RonnieDaza <daza.ronnie.09212002@gmail.com>
2022-11-24 11:00:16 +0800

    Add userInfo

--- Dictionary V2.py
+++ Dictionary V2.py
@@ -10,7 +10,7 @@ dict_info =  {"Mark": ["19", "Manila", "09123456789"]}
 while True:
 
     choice = int(input("What do you want to do? (1-3) "))
-    
+
     if choice == 1:
         fullName = input("Name: ")
         age = int(input("Age: "))
@@ -19,12 +19,15 @@ while True:
         phoneNumber = input("Phone Number: ")
         gender = input("Gender: ")
         email = input("Email: ")
-        userInfo = dict_info[fullName] = {"age": age,
-                             "address": address,
-                             "country": country,
-                             "phone number": phoneNumber,
-                             "gender": gender,
-                             "email": email,
-                             }
-        userList = dict_info.update(userInfo)
+        
+        userInfo = {
+            "Full Name": fullName,
+            "Age": age,
+            "Address": address,
+            "Country": country,
+            "Phone Number": phoneNumber,
+            "Gender": gender,
+            "email": email,
+        }
+        
         print("Saved!")

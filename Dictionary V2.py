3516f9c021613dc2634c11e73d7923ea9c2c9c86
RonnieDaza <daza.ronnie.09212002@gmail.com>
2022-11-24 09:49:39 +0800

    Store data

--- Dictionary V2.py
+++ Dictionary V2.py
@@ -10,6 +10,7 @@ dict_info =  {"Mark": ["19", "Manila", "09123456789"]}
 while True:
 
     choice = int(input("What do you want to do? (1-3) "))
+    
     if choice == 1:
         fullName = input("Name: ")
         age = int(input("Age: "))
@@ -18,3 +19,12 @@ while True:
         phoneNumber = input("Phone Number: ")
         gender = input("Gender: ")
         email = input("Email: ")
+        userInfo = dict_info[fullName] = {"age": age,
+                             "address": address,
+                             "country": country,
+                             "phone number": phoneNumber,
+                             "gender": gender,
+                             "email": email,
+                             }
+        userList = dict_info.update(userInfo)
+        print("Saved!")

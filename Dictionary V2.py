bfd11ad89a1996f7e6ec7a4eaed00d942388a6a9
RonnieDaza <daza.ronnie.09212002@gmail.com>
2022-11-24 09:48:24 +0800

    Add dictionary and choice

---
+++ Dictionary V2.py
@@ -0,0 +1,12 @@
+print("-----------------------------")
+print("MENU:")
+print("1 -> Add an item")
+print("2 -> Search")
+print("3 -> Exit (y/n)")
+print("-----------------------------")
+
+dict_info =  {"Mark": ["19", "Manila", "09123456789"]}
+
+while True:
+
+    choice = int(input("What do you want to do? (1-3) "))
--- Dictionary.py
+++
@@ -1,6 +0,0 @@
-print("-----------------------------")
-print("MENU:")
-print("1 -> Add an item")
-print("2 -> Search")
-print("3 -> Exit (y/n)")
-print("-----------------------------")
